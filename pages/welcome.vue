01515aab57e3ad391912838f2b685784032e3050
Rere Arga Dewanata <70679432+argadewanata@users.noreply.github.com>
2024-12-18 09:12:37 +0700

    feat: support get data using jwt

--- pages/welcome.vue
+++ pages/welcome.vue
@@ -2,20 +2,41 @@
     <div class="min-h-screen flex items-center justify-center bg-gray-50">
         <div class="bg-white p-16 rounded-lg shadow-lg w-full max-w-lg text-center">
             <h2 class="text-2xl font-bold mb-4 text-red-600">Welcome</h2>
-            <p>ID: {{ id }}</p>
-            <p>Nama: {{ name }}</p>
-            <p>Email: {{ email }}</p>
+            <p v-if="user">ID: {{ user.id }}</p>
+            <p v-if="user">Name: {{ user.name }}</p>
+            <p v-if="user">Email: {{ user.email }}</p>
+            <p v-if="errorMessage" class="mt-4 text-red-500">{{ errorMessage }}</p>
         </div>
     </div>
 </template>
 
 <script setup>
-import { useRoute } from '#app'
+import { ref, onMounted } from 'vue'
+import { useRuntimeConfig, useRouter } from '#app'
 
-const route = useRoute()
-const name = route.query.name || ''
-const email = route.query.email || ''
-const id = route.query.id || ''
+const user = ref(null)
+const errorMessage = ref('')
+const runtimeConfig = useRuntimeConfig()
+const ipBE = runtimeConfig.public.ipBE
+const router = useRouter()
+
+onMounted(async () => {
+  try {
+    const token = localStorage.getItem('access_token')
+    if (!token) {
+      throw new Error('No token found')
+    }
+    console.log(token)
+    user.value = await $fetch(`${ipBE}/api/member/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+  } catch (error) {
+    errorMessage.value = 'Failed to fetch user information'
+    console.error('Error fetching user data:', error)
+  }
+})
 </script>
 
 <style scoped></style>
